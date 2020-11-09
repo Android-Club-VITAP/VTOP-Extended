b6eb622784fd31f2c03f4e3b77d10ab9cceafbd0
Amritesh Dasari <amritesh.18bcn7038@vitap.ac.in>
2020-11-09 19:27:00 +0530

    Anonymous sign in test

---
+++ lib/services/auth.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Sign in anon
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+  //Sign in with email and password
+  //Register with email and password
+  //sign out
+}
