621a877c2da133620d270d064614d454cd903cb5
Amritesh Dasari <amritesh.18bcn7038@vitap.ac.in>
2020-11-11 22:26:46 +0530

    Register with Email and Password

--- lib/UI/authenticate/register.dart
+++ lib/UI/authenticate/register.dart
@@ -10,8 +10,10 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formkey = GlobalKey<FormState>();
   String email = '';
   String password = '';
+  String error = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,34 +34,50 @@ class _RegisterState extends State<Register> {
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
           child: Form(
+              key: _formkey,
               child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                  color: Colors.pink[400],
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    validator: (val) => val.isEmpty ? 'Enter an Email' : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    },
                   ),
-                  onPressed: () async {
-                    print(email);
-                    print(password);
-                  })
-            ],
-          ))),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    validator: (val) => val.length < 6
+                        ? 'Enter a password 6+ characters long'
+                        : null,
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  RaisedButton(
+                      color: Colors.pink[400],
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formkey.currentState.validate()) {
+                          dynamic result = await _auth
+                              .registerWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() => error =
+                                'Error registering with given email and password');
+                          } else {}
+                        }
+                      }),
+                      SizedBox(height: 12.0,),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      ),
+                ],
+              ))),
     );
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -27,6 +27,18 @@ class AuthService {
 
   //Sign in with email and password
   //Register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
   //sign out
   Future signOut() async {
     try {
