2cdef5649e10cd6a60505249652f704833de4b03
ADITYA MITRA <adityaarghya0@gmail.com>
2020-11-26 17:50:41 +0530

    Add files via upload

--- lib/UI/homes/vtoppage.dart
+++ lib/UI/homes/vtoppage.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 class VtopPage extends StatefulWidget {
   @override
   _VtopPageState createState() => _VtopPageState();
@@ -11,11 +13,14 @@ class _VtopPageState extends State<VtopPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(5.0),
-        child: WebView(
-          initialUrl: "http://vtop2.vitap.ac.in:8070/vtop/",
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
-      ),
+        child:  InAppWebView(
+        initialUrl: "http://vtop2.vitap.ac.in:8070/vtop/",
+        initialHeaders: {},
+        onWebViewCreated: (InAppWebViewController controller) {
+          //webView = controller;
+        },
+               
+      ),      ),
     );
   }
 }
